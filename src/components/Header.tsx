7ef3e3e43242a37b0843c2f3e8af4e7df311b676
Ryan Van Valkenburg <ryanlvv@gmail.com>
2024-07-12 05:47:45 -0700

    Add links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 type NavItem = {
   label: string;
   href: string;
+  external?: boolean;
 };
 
 const navItems: NavItem[] = [
   { label: 'Home', href: '/' },
-  { label: 'Github', href: '' },
-  { label: 'Vercel', href: '/services' },
+  {
+    label: 'Github',
+    href: 'https://github.com/ry-animal/lit-protocol-next',
+    external: true,
+  },
+  {
+    label: 'Vercel',
+    href: 'https://lit-protocol-next.vercel.app/',
+    external: true,
+  },
 ];
 
 const Header: React.FC = () => {
@@ -22,15 +32,27 @@ const Header: React.FC = () => {
             Genius Swap
           </div>
           <nav className="flex space-x-4 md:gap-10">
-            {navItems.map((item) => (
-              <a
-                key={item.href}
-                href={item.href}
-                className="hover:text-gray-500 transition-colors duration-300"
-              >
-                {item.label}
-              </a>
-            ))}
+            {navItems.map((item) =>
+              item.external ? (
+                <a
+                  key={item.href}
+                  href={item.href}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:text-gray-500 transition-colors duration-300"
+                >
+                  {item.label}
+                </a>
+              ) : (
+                <Link
+                  key={item.href}
+                  href={item.href}
+                  className="hover:text-gray-500 transition-colors duration-300"
+                >
+                  {item.label}
+                </Link>
+              )
+            )}
           </nav>
         </div>
       </div>
