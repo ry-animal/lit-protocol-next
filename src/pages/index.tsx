909e8a5e503cf2e6759f1174679720197829bf04
Ryan Van Valkenburg <ryanlvv@gmail.com>
2024-07-11 17:10:25 -0700

    testing husky

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  console.log('test');
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 max-w-5xl w-full items-center justify-center font-mono text-sm flex">
         Hello Genius
       </div>
     </main>
---
+++ src/utils/lit.ts
@@ -0,0 +1,125 @@
+import { LitNodeClient } from '@lit-protocol/lit-node-client';
+import { GoogleProvider, LitAuthClient } from '@lit-protocol/lit-auth-client';
+import {
+  AuthMethodScope,
+  AuthMethodType,
+  ProviderType,
+} from '@lit-protocol/constants';
+import {
+  AuthMethod,
+  GetSessionSigsProps,
+  IRelayPKP,
+  SessionSigs,
+} from '@lit-protocol/types';
+
+export const litNodeClient: LitNodeClient = new LitNodeClient({
+  alertWhenUnauthorized: false,
+  litNetwork: 'datil-dev',
+  debug: true,
+});
+
+export const litAuthClient: LitAuthClient = new LitAuthClient({
+  litRelayConfig: {
+    relayApiKey: 'test-api-key',
+  },
+  litNodeClient,
+});
+
+export async function signInWithGoogle(redirectUri: string): Promise<void> {
+  const googleProvider = litAuthClient.initProvider<GoogleProvider>(
+    ProviderType.Google,
+    { redirectUri }
+  );
+  await googleProvider.signIn();
+}
+
+export async function authenticateWithGoogle(
+  redirectUri: string
+): Promise<AuthMethod | undefined> {
+  const googleProvider = litAuthClient.initProvider<GoogleProvider>(
+    ProviderType.Google,
+    { redirectUri }
+  );
+  const authMethod = await googleProvider.authenticate();
+  return authMethod;
+}
+
+export async function getSessionSigs({
+  pkpPublicKey,
+  authMethod,
+  sessionSigsParams,
+}: {
+  pkpPublicKey: string;
+  authMethod: AuthMethod;
+  sessionSigsParams: GetSessionSigsProps;
+}): Promise<SessionSigs> {
+  const provider = getProviderByAuthMethod(authMethod);
+  if (provider) {
+    const sessionSigs = await provider.getSessionSigs({
+      pkpPublicKey,
+      authMethod,
+      sessionSigsParams,
+    });
+    return sessionSigs;
+  } else {
+    throw new Error(
+      `provider not found for auth method type ${authMethod.authMethodType}`
+    );
+  }
+}
+
+export async function updateSessionSigs(
+  params: GetSessionSigsProps
+): Promise<SessionSigs> {
+  const sessionSigs = await litNodeClient.getSessionSigs(params);
+  return sessionSigs;
+}
+
+export async function getPKPs(authMethod: AuthMethod): Promise<IRelayPKP[]> {
+  const provider = getProviderByAuthMethod(authMethod);
+  const allPKPs = await provider?.fetchPKPsThroughRelayer(authMethod);
+
+  if (!allPKPs) {
+    throw new Error('Failed to fetch PKPs');
+  }
+
+  return allPKPs;
+}
+
+export async function mintPKP(authMethod: AuthMethod): Promise<IRelayPKP> {
+  const provider = getProviderByAuthMethod(authMethod);
+  const options = {
+    permittedAuthMethodScopes: [[AuthMethodScope.SignAnything]],
+  };
+
+  const txHash = await provider?.mintPKPThroughRelayer(authMethod, options);
+
+  const response = await provider?.relay.pollRequestUntilTerminalState(
+    txHash || ''
+  );
+
+  if (
+    response?.status !== 'Succeeded' ||
+    !response.pkpTokenId ||
+    !response.pkpPublicKey ||
+    !response.pkpEthAddress
+  ) {
+    throw new Error('minting failed');
+  }
+
+  const newPKP: IRelayPKP = {
+    tokenId: response.pkpTokenId,
+    publicKey: response.pkpPublicKey,
+    ethAddress: response.pkpEthAddress,
+  };
+  return newPKP;
+}
+
+function getProviderByAuthMethod(authMethod: AuthMethod) {
+  switch (authMethod.authMethodType) {
+    case AuthMethodType.GoogleJwt:
+      return litAuthClient.getProvider(ProviderType.Google);
+    default:
+      return;
+  }
+}
